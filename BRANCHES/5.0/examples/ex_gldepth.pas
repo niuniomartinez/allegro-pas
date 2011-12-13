f8e2b7541de17d3c34843b0de141e86e47052005
niunio <Niunio@2d5d2200-ff19-4802-86c1-a7e13dd4aade>
2011-12-13 20:55:28 +0000

    + [A5] Added some scafolding, testing and stuff.

---
+++ BRANCHES/5.0/examples/ex_gldepth.pas
@@ -0,0 +1,282 @@
+PROGRAM ex_gldepth;
+(*<An example program showing how to set and use a depth buffer with an OpenGL
+ * display.
+ *
+ * Use arrow keys to rotate, PgUp/PgDown to move closer/farther away.
+ *)
+(* TODO: License. *)
+
+USES
+  Allegro5,
+  al5image,
+  al5gl,
+  al5font,
+  GL;
+
+TYPE
+(* Stores camera information. *)
+  TCamera = RECORD
+  (* Camera angle. *)
+    xAngle, yAngle, zAngle: DOUBLE;
+  (* Camera distance. *)
+    Dist: DOUBLE;
+  END;
+
+
+
+VAR
+(* Camera. *)
+  Camera: TCamera = (
+    xAngle:  0.0;
+    yAngle:  0.0;
+    zAngle:  0.0;
+    Dist  : 20.0;
+  );
+
+CONST
+(* Speed animation. *)
+  AngleSpeed = 5.0;
+  DistSpeed = 1.0;
+
+VAR
+(* Texture. *)
+  Texture: GLuint;
+  Bitmap: ALLEGRO_BITMAP_PTR;
+(* Stores if key is pressed. *)
+  KeyStatus: ARRAY [0..ALLEGRO_KEY_MAX] OF BOOLEAN;
+
+
+
+(* Sets camera position. *)
+  PROCEDURE SetCameraPosition;
+  BEGIN
+    glMatrixMode (GL_PROJECTION);
+    glLoadIdentity;
+    glFrustum (-1.0, 1.0, -1.0, 1.0, 1.0, 40.0);
+    glTranslatef (0, 0, -Camera.Dist);
+    glRotatef (Camera.xAngle, 1, 0, 0);
+    glRotatef (Camera.yAngle, 0, 1, 0);
+    glRotatef (Camera.zAngle, 0, 0, 1);
+    glMatrixMode (GL_MODELVIEW);
+  END;
+
+
+
+(* Process keyboard input. *)
+  PROCEDURE Keyboard;
+  BEGIN
+    IF KeyStatus[ALLEGRO_KEY_LEFT] THEN
+      INC (Camera.yAngle, AngleSpeed);
+    IF KeyStatus[ALLEGRO_KEY_RIGHT] THEN
+      INC (Camera.yAngle, AngleSpeed);
+
+    IF KeyStatus[ALLEGRO_KEY_UP] THEN
+      INC (Camera.xAngle, AngleSpeed);
+    IF KeyStatus[ALLEGRO_KEY_DOWN] THEN
+      INC (Camera.xAngle, AngleSpeed);
+
+    IF KeyStatus[ALLEGRO_KEY_PGUP] THEN
+      INC (Camera.Dist, DistSpeed);
+    IF KeyStatus[ALLEGRO_KEY_PGDN] THEN
+      INC (Camera.Dist, DistSpeed);
+  END;
+
+
+
+(* Draws the scene. *)
+  PROCEDURE DrawScene
+  BEGIN
+  { Clear the RGB buffer and the depth buffer. }
+    glClear (GL_COLOR_BUFFER_BIT OR GL_DEPTH_BUFFER_BIT);
+
+  { Set the modelview matrix to be the identity matrix. }
+    glLoadIdentity;
+  { Translate and rotate the object. }
+    glTranslatef (-2.5, 0.0, 0.0);
+    glRotatef (-30, 1.0, 0.0, 0.0);
+    glRotatef (30, 0.0, 1.0, 0.0);
+    glRotatef (30, 0.0, 0.0, 1.0);
+
+    glColor3f (1.0, 0.0, 1.0);
+
+  { Draw the sides of the three-sided pyramid. }
+    glEnable (GL_TEXTURE_2D);
+    glBindTexture (GL_TEXTURE_2D, tex);
+    glBegin (GL_TRIANGLE_FAN);
+      glTexCoord2f (0, 0); glVertex3d ( 0,  4,  0);
+      glTexCoord2f (1, 0); glVertex3d ( 0, -4, -4);
+      glTexCoord2f (1, 1); glVertex3d (-4, -4,  4);
+      glTexCoord2f (0, 1); glVertex3d ( 4, -4,  4);
+      glTexCoord2f (1, 0); glVertex3d ( 0, -4, -4);
+    glEnd;
+
+    glColor3f (0.0, 1.0, 1.0);
+
+  { Draw the base of the pyramid. }
+    glBegin (GL_TRIANGLES);
+      glTexCoord2f (1, 0); glVertex3d ( 0, -4, -4);
+      glTexCoord2f (0, 1); glVertex3d ( 4, -4,  4);
+      glTexCoord2f (1, 1); glVertex3d (-4, -4,  4);
+    glEnd;
+
+
+    glLoadIdentity;
+    glTranslatef (2.5, 0.0, 0.0);
+    glRotatef (45, 1.0, 0.0, 0.0);
+    glRotatef (45, 0.0, 1.0, 0.0);
+    glRotatef (45, 0.0, 0.0, 1.0);
+
+    glColor3f (0.0, 1.0, 0.0);
+
+    glDisable (GL_TEXTURE_2D);
+  { Draw the sides of the cube. }
+    glBegin (GL_QUAD_STRIP);
+      glVertex3d ( 3,  3, -3);
+      glVertex3d ( 3, -3, -3);
+      glVertex3d (-3,  3, -3);
+      glVertex3d (-3, -3, -3);
+      glVertex3d (-3,  3,  3);
+      glVertex3d (-3, -3,  3);
+      glVertex3d ( 3,  3,  3);
+      glVertex3d ( 3, -3,  3);
+      glVertex3d ( 3,  3, -3);
+      glVertex3d ( 3, -3, -3);
+    glEnd;
+
+    glColor3f (0.0, 0.0, 1.0);
+
+  { Draw the top of the cube. }
+    glBegin (GL_QUADS);
+      glVertex3d (-3, -3, -3);
+      glVertex3d ( 3, -3, -3);
+      glVertex3d ( 3, -3,  3);
+      glVertex3d (-3, -3,  3);
+    glEnd;
+
+  { Bottom is texture-mapped. }
+    glEnable (GL_TEXTURE_2D);
+    glBindTexture (GL_TEXTURE_2D, tex);
+    glBegin (GL_QUADS);
+      glTexCoord2f (0, 0); glVertex3d (-3,  3, -3);
+      glTexCoord2f (1, 0); glVertex3d (-3,  3,  3);
+      glTexCoord2f (1, 1); glVertex3d ( 3,  3,  3);
+      glTexCoord2f (0, 1); glVertex3d ( 3,  3, -3);
+    glEnd;
+  END;
+
+
+
+(* Loads and adds the textures. *)
+  PROCEDURE SetupTextures (Display ALLEGRO_DISPLAY_PTR);
+  VAR
+    tmpBmp: ALLEGRO_BITMAP_PTR;
+    Font: ALLEGRO_FONT_PTR;
+    w, h, Depth: INTEGER;
+  BEGIN
+    Font := al_load_font ('data/fixed_font.tga', 0, 0);
+    IF Font = NIL THEN
+    BEGIN
+      abort_example ('Error loading `data/fixed_font.tga');
+      exit (1);
+    END;
+
+    tmpBmp := al_load_bitmap ('data/mysha.pcx');
+    IF tmpBmp = NIL THEN
+    BEGIN
+      abort_example ('Error loading ''data/mysha.pcx''');
+      exit (1);
+    END;
+    w := 128;
+    h := 128;
+    Bitmap := al_create_bitmap (w, h);
+    al_set_target_bitmap (Bitmap);
+    al_draw_scaled_bitmap (tmpBmp,
+      0, 0, al_get_bitmap_width (Bitmap), al_get_bitmap_height (Bitmap),
+      0, 0, w, h, 0);
+
+    Depth = al_get_display_option (Display, ALLEGRO_DEPTH_SIZE);
+    IF Depth = 0 THEN
+      al_draw_textf (Font, al_map_rgb (255, 0, 0), 0, 5, 0, 'No Z-buffer!')
+    ELSE
+      al_draw_textf (Font, al_map_rgb (255, 0, 0), 0, 5, 0, 'Z-buffer: %i bits', Depth);
+    al_set_target_backbuffer (Display);
+    al_destroy_bitmap (tmp_bmp);
+    al_destroy_font (Font);
+
+    glEnable (GL_TEXTURE_2D);
+    glTexEnvi (GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_DECAL);
+
+    Texture = al_get_opengl_texture (Bitmap);
+  END;
+
+
+
+VAR
+  Display: ALLEGRO_DISPLAY_PTR;
+  Queue: ALLEGRO_EVENT_QUEUE_PTR;
+  Timer: ALLEGRO_TIMER_PTR;
+  Event: ALLEGRO_EVENT_PTR;
+  DoLoop: BOOLEAN;
+BEGIN
+{ Inits Allegro. }
+  IF NOT al_init THEN
+  BEGIN
+    abort_example ('Could not init Allegro.');
+    EXIT;
+  END;
+
+  al_init_image_addon;
+  al_init_font_addon;
+  al_install_keyboard;
+
+  al_set_new_display_flags (ALLEGRO_OPENGL);
+  al_set_new_display_option (ALLEGRO_DEPTH_SIZE, 16, ALLEGRO_SUGGEST);
+  Display = al_create_display (640, 480);
+  IF Display = NIL THEN
+  BEGIN
+    abort_example ('Could not create display.');
+    EXIT;
+  END;
+
+  Timer = al_create_timer (1. / 60.);
+
+  Queue = al_create_event_queue;
+  al_register_event_source (Queue,al_get_keyboard_event_source);
+  al_register_event_source (Queue,al_get_display_event_source (Display));
+  al_register_event_source (Queue,al_get_timer_event_source (Timer));
+
+  glEnable (GL_DEPTH_TEST);
+  glDisable (GL_CULL_FACE);
+
+  SetupTextures (Display);
+  al_start_timer (Timer);
+
+  DoLoop := TRUE;
+  WHILE DoLoop DO
+  BEGIN
+    al_wait_for_event (Queue, &Event);
+    CASE Event.type OF
+    ALLEGRO_EVENT_DISPLAY_CLOSE:
+      DoLoop := FALSE;
+    ALLEGRO_EVENT_KEY_DOWN:
+      BEGIN
+	IF Event.keyboard.keycode = ALLEGRO_KEY_ESCAPE THEN
+	  DoLoop := FALSE;
+	KeyStatus[Event.keyboard.keycode] := true;
+      END;
+    ALLEGRO_EVENT_KEY_UP:
+      KeyStatus[Event.keyboard.keycode] := false;
+    ALLEGRO_EVENT_TIMER:
+      BEGIN
+	Keyboard;
+	IF al_is_event_queue_empty (Queue) THEN
+	BEGIN
+	  SetCameraPosition;
+	  draw;
+	  al_flip_display;
+	END;
+      END;
+    END;
+  END;
+  al_destroy_bitmap (Bitmap);
+END.
