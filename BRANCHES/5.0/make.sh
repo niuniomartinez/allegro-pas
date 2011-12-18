6e709fdb2bb6d9aaf5218b99f03c4562ba86cb9d
niunio <niunio@2d5d2200-ff19-4802-86c1-a7e13dd4aade>
2011-12-18 13:17:09 +0000

    [A5]  + More stuff...  + I decided to separate the unit code into sections, one per header file.

--- BRANCHES/5.0/make.sh
+++ BRANCHES/5.0/make.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-fpc -g -Fulib -FUobj -FEexamples examples/test.pas
-#fpc -g -Fulib -FUobj -FEexamples examples/ex_gldepth.pas
+#fpc -g -Fulib -FUobj -FEexamples examples/test.pas
+fpc -g -Fulib -FUobj -FEexamples examples/ex_gldepth.pas
