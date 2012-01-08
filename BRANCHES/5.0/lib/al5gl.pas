333d3a0b37a44559ddc18bb6c901b71fa239dd7d
niunio <niunio@2d5d2200-ff19-4802-86c1-a7e13dd4aade>
2012-01-08 19:32:53 +0000

    [A5]  + Added the last stuff to unit allegro5.pas (last at the moment, may be I'll    add more in the future).  * Unit al5gl.pas completed.

--- BRANCHES/5.0/lib/al5gl.pas
+++ BRANCHES/5.0/lib/al5gl.pas
@@ -1,5 +1,18 @@
 UNIT al5gl;
-(*<Interfaces between Allegro and OpenGL. *)
+(*         ______   ___    ___
+ *        /\  _  \ /\_ \  /\_ \
+ *        \ \ \L\ \\//\ \ \//\ \      __     __   _ __   ___
+ *         \ \  __ \ \ \ \  \ \ \   /'__`\ /'_ `\/\`'__\/ __`\
+ *          \ \ \/\ \ \_\ \_ \_\ \_/\  __//\ \L\ \ \ \//\ \L\ \
+ *           \ \_\ \_\/\____\/\____\ \____\ \____ \ \_\\ \____/
+ *            \/_/\/_/\/____/\/____/\/____/\/___L\ \/_/ \/___/
+ *                                           /\____/
+ *                                           \_/__/
+ *
+ * Interfaces between Allegro and OpenGL.
+ *
+ *      See readme.txt for copyright information.
+ *)
 
 INTERFACE
 
@@ -9,21 +22,66 @@ INTERFACE
 
 {$include allegro.cfg}
 
-(* Returns the OpenGL texture id internally used by the given bitmap if it uses one, else 0.
+(******************************************************************************
+ * opengl/gl_ext.h *
+ ********************)
 
-   Example:
-@longcode(#
-Bitmap := al_load_bitmap ('my_texture.png');
-Texture := al_get_opengl_texture (Bitmap);
-IF Texture <> 0 THEN
-  glBindTexture (GL_TEXTURE_2D, Texture);
-#)
- *)
+{ TODO: The original extension system uses the C preprocessor to create the
+    structs and constants used.  It's quite complex so I decided to delay it. }
+
+
+
+(******************************************************************************
+ * allegro_opengl.h *
+ ********************)
+
+  TYPE
+    ALLEGRO_OPENGL_VARIANT = (
+      ALLEGRO_DESKTOP_OPENGL = 0,
+      ALLEGRO_OPENGL_ES
+    );
+
+  FUNCTION al_get_opengl_version: LONGWORD; CDECL;
+  FUNCTION al_have_opengl_extension (CONST extension: PCHAR): BOOLEAN; CDECL;
+  FUNCTION al_get_opengl_proc_address (CONST name: PCHAR): POINTER;
   FUNCTION al_get_opengl_texture (bitmap: ALLEGRO_BITMAPptr): GLuint; CDECL;
+  PROCEDURE al_remove_opengl_fbo (bitmap: ALLEGRO_BITMAPptr); CDECL;
+  FUNCTION al_get_opengl_fbo (bitmap: ALLEGRO_BITMAPptr): GLuint; CDECL;
+  PROCEDURE al_get_opengl_texture_size (bitmap: ALLEGRO_BITMAPptr; VAR w, h: LONGINT); CDECL;
+  PROCEDURE al_get_opengl_texture_position (bitmap: ALLEGRO_BITMAPptr; VAR u, v: LONGINT); CDECL;
+  PROCEDURE al_set_current_opengl_context (display: ALLEGRO_DISPLAYptr); CDECL;
+  FUNCTION al_get_opengl_variant: LONGINT; CDECL;
 
 IMPLEMENTATION
 
+  FUNCTION al_get_opengl_version: LONGWORD; CDECL;
+  EXTERNAL ALLEGRO_LIB_NAME;
+
+  FUNCTION al_have_opengl_extension (CONST extension: PCHAR): BOOLEAN; CDECL;
+  EXTERNAL ALLEGRO_LIB_NAME;
+
+  FUNCTION al_get_opengl_proc_address (CONST name: PCHAR): POINTER;
+  EXTERNAL ALLEGRO_LIB_NAME;
+
   FUNCTION al_get_opengl_texture (bitmap: ALLEGRO_BITMAPptr): GLuint; CDECL;
+  EXTERNAL ALLEGRO_LIB_NAME;
+
+  PROCEDURE al_remove_opengl_fbo (bitmap: ALLEGRO_BITMAPptr); CDECL;
+  EXTERNAL ALLEGRO_LIB_NAME;
+
+  FUNCTION al_get_opengl_fbo (bitmap: ALLEGRO_BITMAPptr): GLuint; CDECL;
+  EXTERNAL ALLEGRO_LIB_NAME;
+
+  PROCEDURE al_get_opengl_texture_size (bitmap: ALLEGRO_BITMAPptr; VAR w, h: LONGINT); CDECL;
+  EXTERNAL ALLEGRO_LIB_NAME;
+
+  PROCEDURE al_get_opengl_texture_position (bitmap: ALLEGRO_BITMAPptr; VAR u, v: LONGINT); CDECL;
+  EXTERNAL ALLEGRO_LIB_NAME;
+
+  PROCEDURE al_set_current_opengl_context (display: ALLEGRO_DISPLAYptr); CDECL;
+  EXTERNAL ALLEGRO_LIB_NAME;
+
+  FUNCTION al_get_opengl_variant: LONGINT; CDECL;
   EXTERNAL ALLEGRO_LIB_NAME;
 
 END.
