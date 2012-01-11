c863708d0b34a298db50b868ac67a72e4fc31a24
niunio <niunio@2d5d2200-ff19-4802-86c1-a7e13dd4aade>
2012-01-11 16:50:22 +0000

    [A5]   + Added LICENSE and expanded README.   * Fixed "acodec" lib name constant.   * Fixed OpenGL unit name at "al5gl.pas".   + Added examples "ex_transform" and "ex_warp_mouse".

--- BRANCHES/5.0/make.sh
+++ BRANCHES/5.0/make.sh
@@ -4,5 +4,7 @@
 #fpc -g -Fulib -FUobj -FEexamples examples/ex_font.pas
 #fpc -O3 -Fulib -FUobj -FEexamples examples/ex_haiku.pas
 #fpc -g -Fulib -FUobj -FEexamples examples/ex_lines.pas
-fpc -g -Fulib -FUobj -FEexamples examples/ex_prim.pas
+#fpc -g -Fulib -FUobj -FEexamples examples/ex_prim.pas
+#fpc -g -Fulib -FUobj -FEexamples examples/ex_warp_mouse.pas
+fpc -g -Fulib -FUobj -FEexamples examples/ex_transform.pas
 
