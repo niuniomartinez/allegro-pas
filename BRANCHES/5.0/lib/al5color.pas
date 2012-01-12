8b09e2a0ca1689d4438c163f0844eb739c9260c3
niunio <Niunio@2d5d2200-ff19-4802-86c1-a7e13dd4aade>
2012-01-12 22:04:09 +0000

    [A5]  * Fixed some procedures at "al5color.pas".  + Added "tsl" to "allegro.pas".  + Added example "ex_blit.pas", but only part translated.

--- BRANCHES/5.0/lib/al5color.pas
+++ BRANCHES/5.0/lib/al5color.pas
@@ -27,12 +27,12 @@ INTERFACE
   PROCEDURE al_color_hsl_to_rgb (h, s, l: SINGLE; VAR r, g, b: SINGLE); CDECL;
   FUNCTION al_color_name_to_rgb (CONST name: STRING; VAR r, g, b: SINGLE): BOOLEAN; CDECL;
   FUNCTION al_color_rgb_to_name (r, g, b: SINGLE): PCHAR; CDECL;
-  PROCEDURE al_color_cmyk_to_rgb, (c, m, y, k: SINGLE; VAR r, g, b: SINGLE); CDECL;
+  PROCEDURE al_color_cmyk_to_rgb (c, m, y, k: SINGLE; VAR r, g, b: SINGLE); CDECL;
   PROCEDURE al_color_rgb_to_cmyk (r, g, b: SINGLE; VAR c, m, y, k: SINGLE); CDECL;
-  PROCEDURE al_color_yuv_to_rgb, (y, u, v: SINGLE;  VAR r, g, b: SINGLE); CDECL;
+  PROCEDURE al_color_yuv_to_rgb (y, u, v: SINGLE;  VAR r, g, b: SINGLE); CDECL;
   PROCEDURE al_color_rgb_to_yuv (r, g, b: SINGLE; VAR y, u, v: SINGLE); CDECL;
   PROCEDURE al_color_rgb_to_html (r, g, b: SINGLE; str: STRING); CDECL;
-  PROCEDURE al_color_html_to_rgb, (CONST str: STRING; VAR r, g, b: SINGLE); CDECL;
+  PROCEDURE al_color_html_to_rgb (CONST str: STRING; VAR r, g, b: SINGLE); CDECL;
   FUNCTION al_color_yuv (y, u, v: SINGLE): ALLEGRO_COLOR; CDECL;
   FUNCTION al_color_cmyk (c, m, y, k: SINGLE): ALLEGRO_COLOR; CDECL;
   FUNCTION al_color_hsl (h, s, l: SINGLE): ALLEGRO_COLOR; CDECL;
@@ -63,13 +63,13 @@ IMPLEMENTATION
   FUNCTION al_color_rgb_to_name (r, g, b: SINGLE): PCHAR; CDECL;
   EXTERNAL ALLEGRO_COLOR_LIB_NAME;
 
-  PROCEDURE al_color_cmyk_to_rgb, (c, m, y, k: SINGLE; VAR r, g, b: SINGLE); CDECL;
+  PROCEDURE al_color_cmyk_to_rgb (c, m, y, k: SINGLE; VAR r, g, b: SINGLE); CDECL;
   EXTERNAL ALLEGRO_COLOR_LIB_NAME;
 
   PROCEDURE al_color_rgb_to_cmyk (r, g, b: SINGLE; VAR c, m, y, k: SINGLE); CDECL;
   EXTERNAL ALLEGRO_COLOR_LIB_NAME;
 
-  PROCEDURE al_color_yuv_to_rgb, (y, u, v: SINGLE;  VAR r, g, b: SINGLE); CDECL;
+  PROCEDURE al_color_yuv_to_rgb (y, u, v: SINGLE;  VAR r, g, b: SINGLE); CDECL;
   EXTERNAL ALLEGRO_COLOR_LIB_NAME;
 
   PROCEDURE al_color_rgb_to_yuv (r, g, b: SINGLE; VAR y, u, v: SINGLE); CDECL;
@@ -78,7 +78,7 @@ IMPLEMENTATION
   PROCEDURE al_color_rgb_to_html (r, g, b: SINGLE; str: STRING); CDECL;
   EXTERNAL ALLEGRO_COLOR_LIB_NAME;
 
-  PROCEDURE al_color_html_to_rgb, (CONST str: STRING; VAR r, g, b: SINGLE); CDECL;
+  PROCEDURE al_color_html_to_rgb (CONST str: STRING; VAR r, g, b: SINGLE); CDECL;
   EXTERNAL ALLEGRO_COLOR_LIB_NAME;
 
   FUNCTION al_color_yuv (y, u, v: SINGLE): ALLEGRO_COLOR; CDECL;
