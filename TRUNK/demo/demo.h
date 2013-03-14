763ed0b43ecb940e8aec79db85b858bae0531213
niunio <niunio@2d5d2200-ff19-4802-86c1-a7e13dd4aade>
2013-03-14 11:23:20 +0000

    [4.4] * Tiles used by demo are now stored in a single big bitmap.  This makes it   easer to adapt the engine and the editor to custom projects. * Some documentation fixes.

--- TRUNK/demo/demo.h
+++ TRUNK/demo/demo.h
@@ -1,45 +1,39 @@
 /* Allegro datafile object indexes, produced by grabber v4.4.2, Unix */
-/* Datafile: /home/guillermo/prog/bibliotecas/allegro-pas/allegro.pas.TRUNK/demo/demo.dat */
-/* Date: Mon Dec 10 19:21:11 2012 */
+/* Datafile: /home/guillermo/Trabajo/bibliotecas/allegro.pas/allegro-pas/demo/demo.dat */
+/* Date: Thu Mar 14 11:44:23 2013 */
 /* Do not hand edit! */
 
-#define BMP_COIN                         0        /* BMP  */
-#define BMP_BLK1                         1        /* BMP  */
-#define BMP_BLK2                         2        /* BMP  */
-#define BMP_BLK3                         3        /* BMP  */
-#define BMP_ROW_R                        4        /* BMP  */
-#define BMP_ROW_L                        5        /* BMP  */
-#define BMP_ROW_D                        6        /* BMP  */
-#define BMP_BOMB1                        7        /* RLE  */
-#define BMP_BOMB2                        8        /* RLE  */
-#define BMP_EXPLOSION1                   9        /* RLE  */
-#define BMP_EXPLOSION2                   10       /* RLE  */
-#define BMP_MAIN_L0                      11       /* RLE  */
-#define BMP_MAIN_L1                      12       /* RLE  */
-#define BMP_MAIN_L2                      13       /* RLE  */
-#define BMP_MAIN_L3                      14       /* RLE  */
-#define BMP_MAIN_LD                      15       /* RLE  */
-#define BMP_MAIN_LJ                      16       /* RLE  */
-#define BMP_MAIN_R0                      17       /* RLE  */
-#define BMP_MAIN_R1                      18       /* RLE  */
-#define BMP_MAIN_R2                      19       /* RLE  */
-#define BMP_MAIN_R3                      20       /* RLE  */
-#define BMP_MAIN_RD                      21       /* RLE  */
-#define BMP_MAIN_RJ                      22       /* RLE  */
-#define GAME_FONT                        23       /* FONT */
-#define GAME_MUSIC                       24       /* SAMP */
-#define GAME_PAL                         25       /* PAL  */
-#define INTRO_ANIM                       26       /* FLIC */
-#define INTRO_MUSIC                      27       /* MIDI */
-#define INTRO_SPL                        28       /* SAMP */
-#define SND_COIN                         29       /* SAMP */
-#define SND_DEAD                         30       /* SAMP */
-#define SND_EXPLODE                      31       /* SAMP */
-#define SND_JUMP                         32       /* SAMP */
-#define SND_WIN                          33       /* SAMP */
-#define TITLE_BMP                        34       /* BMP  */
-#define TITLE_FONT                       35       /* FONT */
-#define TITLE_MUSIC                      36       /* MIDI */
-#define TITLE_PAL                        37       /* PAL  */
-#define WELCOME_SPL                      38       /* SAMP */
+#define BMP_TILES                        0        /* BMP  */
+#define BMP_BOMB1                        1        /* RLE  */
+#define BMP_BOMB2                        2        /* RLE  */
+#define BMP_EXPLOSION1                   3        /* RLE  */
+#define BMP_EXPLOSION2                   4        /* RLE  */
+#define BMP_MAIN_L0                      5        /* RLE  */
+#define BMP_MAIN_L1                      6        /* RLE  */
+#define BMP_MAIN_L2                      7        /* RLE  */
+#define BMP_MAIN_L3                      8        /* RLE  */
+#define BMP_MAIN_LD                      9        /* RLE  */
+#define BMP_MAIN_LJ                      10       /* RLE  */
+#define BMP_MAIN_R0                      11       /* RLE  */
+#define BMP_MAIN_R1                      12       /* RLE  */
+#define BMP_MAIN_R2                      13       /* RLE  */
+#define BMP_MAIN_R3                      14       /* RLE  */
+#define BMP_MAIN_RD                      15       /* RLE  */
+#define BMP_MAIN_RJ                      16       /* RLE  */
+#define GAME_FONT                        17       /* FONT */
+#define GAME_MUSIC                       18       /* SAMP */
+#define GAME_PAL                         19       /* PAL  */
+#define INTRO_ANIM                       20       /* FLIC */
+#define INTRO_MUSIC                      21       /* MIDI */
+#define INTRO_SPL                        22       /* SAMP */
+#define SND_COIN                         23       /* SAMP */
+#define SND_DEAD                         24       /* SAMP */
+#define SND_EXPLODE                      25       /* SAMP */
+#define SND_JUMP                         26       /* SAMP */
+#define SND_WIN                          27       /* SAMP */
+#define TITLE_BMP                        28       /* BMP  */
+#define TITLE_FONT                       29       /* FONT */
+#define TITLE_MUSIC                      30       /* MIDI */
+#define TITLE_PAL                        31       /* PAL  */
+#define WELCOME_SPL                      32       /* SAMP */
 
