dc54c7454c294ba8fdd8fa8f1daaadbb94b2185c
niunio <Niunio@2d5d2200-ff19-4802-86c1-a7e13dd4aade>
2014-09-15 13:37:08 +0000

    [alGUI]  + Added new properties and methods to TalGUI_Control class, including    initialization, finalization and mouse control.  * Changed TalGUI_Control property "Disabled" to "Enabled".  It looks more like    Delphi and Lazarus components.  + Added a lot of functionality to TalGUI_Dialog.  Actually, it's able to    manage (simple) dialogs and send the apropriate messages to the controls.    May be it nees some tuning, but it works.  + Added default behavior to TalGUI_CustomButton.  This includes an "onClick"    event that works like RAD ones (i.e. Delphi and Lazarus).  + Test program is more object-oriented.

--- TRUNK/addons/alGUI/test.pas
+++ TRUNK/addons/alGUI/test.pas
@@ -2,45 +2,136 @@ PROGRAM Test;
 (* Tests the GUI. *)
 
   USES
-    allegro, alGUI,
+    allegro, alGUI, alGUICommonCtrls,
     sysutils;
 
-  VAR
-    Styles: ARRAY [0..0] OF TalGUI_Style;
-    Ndx, ActiveStyle: INTEGER;
-    Dialog: TalGUI_Dialog;
-BEGIN
-  IF NOT al_init THEN
+  TYPE
+  (* Defines an application class. *)
+    TDemoGUI = CLASS (TObject)
+    PRIVATE
+      Dialog: TalGUI_Dialog;
+    (* Event executed when clicking the close button. *)
+      PROCEDURE onCloseBtnClick (Sender: TalGUI_Control);
+    PUBLIC
+    (* Constructor. *)
+      CONSTRUCTOR Create;
+    (* Destructor. *)
+      DESTRUCTOR Destroy; OVERRIDE;
+    (* Initializes the application. *)
+      PROCEDURE Initialize;
+    (* Executes de application. *)
+      PROCEDURE Run; INLINE;
+    END;
+
+
+
+(* Event executed when clicking the close button. *)
+  PROCEDURE TDemoGUI.onCloseBtnClick (Sender: TalGUI_Control);
   BEGIN
-    WriteLn ('Can''t init Allegro.');
-    EXIT;
+    Dialog.Close
   END;
 
-  al_install_timer;
-  al_install_keyboard;
-  al_install_mouse;
 
-  al_set_color_depth (32);
-  IF NOT al_set_gfx_mode (AL_GFX_AUTODETECT_WINDOWED, 640, 480, 0, 0) THEN
-    IF NOT al_set_gfx_mode (AL_GFX_SAFE, 320, 200, 0, 0) THEN
+
+(* Constructor. *)
+  CONSTRUCTOR TDemoGUI.Create;
+  BEGIN
+    INHERITED Create;
+    Dialog := NIL
+  END;
+
+
+
+(* Destructor. *)
+  DESTRUCTOR TDemoGUI.Destroy;
+  BEGIN
+    Dialog.Free;
+    INHERITED Destroy
+  END;
+
+
+
+(* Initializes the application. *)
+  PROCEDURE TDemoGUI.Initialize;
+  VAR
+    Ndx: INTEGER;
+  BEGIN
+  { Initialize Allegro. }
+    IF NOT al_init THEN
     BEGIN
-      al_set_gfx_mode (AL_GFX_TEXT, 0, 0, 0, 0);
-      al_message ('Unable to set any graphic mode'#10+al_error+''#10);
-      EXIT
+      WriteLn ('Can''t init Allegro.');
+      EXIT;
     END;
-  al_show_mouse (al_screen);
-
-  TRY
+    al_install_timer;
+    al_install_keyboard;
+    al_install_mouse;
+    al_set_color_depth (al_desktop_color_depth);
+    IF NOT al_set_gfx_mode (AL_GFX_AUTODETECT_WINDOWED, 640, 480, 0, 0) THEN
+      IF NOT al_set_gfx_mode (AL_GFX_SAFE, 320, 200, 0, 0) THEN
+	RAISE Exception.Create ('Unable to set any graphic mode'#10+al_error+''#10);
+    al_show_mouse (al_screen);
+  { Dialog must be created after setting the graphics mode. }
     Dialog := TalGUI_Dialog.Create;
+ { Play fair with OS. }
+    Ndx := Dialog.Controls.Add (TalGUI_ClearScreen.Create);
+    TalGUI_ClearScreen (Dialog.Controls[Ndx]).Color :=
+      Dialog.Style.BackgroundColor;
 
-    Dialog.Controls.Add (TalGUI_ClearScreen.Create); { Play fair with OS. }
+    Ndx := Dialog.Controls.Add (TalGUI_Button.Create);
+    WITH TalGUI_Button (Dialog.Controls[Ndx]) DO
+    BEGIN
+      X := 25; Y := 25;
+      Width := 100; Height := 16;
+      Caption := 'A button';
+    END;
+    Ndx := Dialog.Controls.Add (TalGUI_Button.Create);
+    WITH TalGUI_Button (Dialog.Controls[Ndx]) DO
+    BEGIN
+      X := 25; Y := 125;
+      Width := 100; Height := 16;
+      Caption := 'Another Btn';
+    END;
+ { Close button. }
+    Ndx := Dialog.Controls.Add (TalGUI_Button.Create);
+    WITH TalGUI_Button (Dialog.Controls[Ndx]) DO
+    BEGIN
+      X := 25; Y := 200;
+      Width := 100; Height := 16;
+      Caption := 'Close dialog';
+    { Set event. }
+      onCLick := @SELF.onCloseBtnClick
+    END;
+  END;
 
-    Styles[0] := Dialog.Style;
-    ActiveStyle := 0;
+
+
+(* Executes de application. *)
+  PROCEDURE TDemoGUI.Run;
+  BEGIN
+    TRY
+      Dialog.Run (1)
+    EXCEPT
+      ON Error: Exception DO
+      BEGIN
+	al_set_gfx_mode (AL_GFX_TEXT, 0, 0, 0, 0);
+        al_message (Error.Message)
+      END
+    END
+  END;
+
+
+
+
+VAR
+  Demo: TDemoGUI;
+BEGIN
+  Demo := TDemoGUI.Create;
+  Demo.Initialize;
+  Demo.Run;
+  FreeAndNil (Demo);
+
 
     {
-    al_clear_to_color (Bmp, Dialog.Style.TextColor);
-
     Dialog.Style.DrawDialogFrame (
       Dialog.Bmp, 5, 5, 315, 195, Dialog.Style.BackgroundColor, 'GUI example', TRUE
     );
@@ -63,18 +154,4 @@ BEGIN
 
     Dialog.Style.DrawBevel (Dialog.Bmp, 100, 50, 130, 100, TRUE);
     }
-
-    Dialog.Run (-1);
-  EXCEPT
-    ON Error: Exception DO
-    BEGIN
-      al_set_gfx_mode (AL_GFX_TEXT, 0, 0, 0, 0);
-      al_message (Error.Message);
-    END
-  END;
-
-  Dialog.Style := NIL; { Avoids destruction by dialog. }
-  FreeAndNil (Dialog);
-  FOR Ndx := LOW (Styles) TO HIGH (Styles) DO
-    IF Styles[Ndx] <> NIL THEN Styles[Ndx].Free
 END.
